d4c3024e65a98fcbd49ca76f96d8a65baae29328
Arturo <nevarez.arturo@uabc.edu.mx>
2020-03-17 16:43:16 -0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainJumbotronComponent } from './components/main-jumbotron/main-jumbotron.component';
+import { AboutComponent } from './components/about/about.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { BioComponent } from './components/bio/bio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainJumbotronComponent,
+    AboutComponent,
+    PartnersComponent,
+    BioComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/main-jumbotron/main-jumbotron.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-jumbotron',
+  templateUrl: './main-jumbotron.component.html',
+  styles: []
+})
+export class MainJumbotronComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
